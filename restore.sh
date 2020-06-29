ce1e0d3b0ac378c8e18a9d31cf454e55a9f3be65
Massimiliano Iacono <massimiliano.iacono@iit.it>
2020-06-29 15:29:44 +0200

    Split bashrc in two for docker related stuff

--- restore.sh
+++ restore.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
+
+. bashrc
+
 # BASHRC
 if [ -f "$HOME/.bashrc" ]; then
     echo "Found pre-existing bashrc. Removing it" 
     rm ~/.bashrc
 fi
 ln bashrc ~/.bashrc
+
+if [ `is_installed docker-ce-cli` -eq "1" ]; then
+	if [ -f "$HOME/.bashrc_docker" ]; then
+	    echo "Found pre-existing bashrc_docker. Removing it" 
+	    rm ~/.bashrc_docker
+    fi
+    ln bashrc_docker ~/.bashrc_docker
+fi
+
 source ~/.bashrc
 
 # SSH
