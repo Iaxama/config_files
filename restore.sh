50c527e96359c9e71063bbf2a44a6384e2c88794
Massimiliano Iacono <massimiliano.iacono@iit.it>
2020-06-29 14:42:07 +0200

    Fixed homedirectory

--- restore.sh
+++ restore.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 # BASHRC
-if [ -f "~/.bashrc" ]; then
-   echo "Found pre-existing bashrc. Removing it" 
-   rm ~/.bashrc
+if [ -f "$HOME/.bashrc" ]; then
+    echo "Found pre-existing bashrc. Removing it" 
+    rm ~/.bashrc
 fi
 ln bashrc ~/.bashrc
 source ~/.bashrc
 
 # SSH
-if [ -d "~/.ssh" ]; then
-   rm ~/.ssh/config
-   ln ssh_config ~/.ssh/config
+if [ -d "$HOME/.ssh" ]; then
+    echo "Found pre-existing .ssh/config. Removing it"
+    rm ~/.ssh/config
+    ln ssh_config ~/.ssh/config
 fi
