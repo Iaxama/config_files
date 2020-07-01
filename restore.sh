b0098efaf370bba310d28275cb203e03a709872a
Massimiliano Iacono <massimiliano.iacono@iit.it>
2020-07-01 15:40:26 +0200

    Added profile file

--- restore.sh
+++ restore.sh
@@ -1,19 +1,34 @@
 #!/bin/bash
 
+check_sudo(){
+    if [[ $UID != 0 ]]; then
+        echo "Please run this script with sudo:"
+        echo "sudo $0 $*"
+        exit 1
+    fi
+}
+
 . bashrc
 
 # BASHRC
 if [ -f "$HOME/.bashrc" ]; then
-    echo "Found pre-existing bashrc. Removing it" 
+    echo "Found pre-existing bashrc. Replacing it" 
     rm ~/.bashrc
 fi
 ln bashrc ~/.bashrc
 
 if [ `is_installed docker-ce-cli` -eq "1" ]; then
-	if [ -f "$HOME/.bashrc_docker" ]; then
-	    echo "Found pre-existing bashrc_docker. Removing it" 
-	    rm ~/.bashrc_docker
+    if [ -f "$HOME/.bashrc_docker" ]; then
+        echo "Found pre-existing bashrc_docker. Replacing it" 
+        rm ~/.bashrc_docker
     fi
+
+    if [ -f "/etc/profile" ]; then
+        echo "Found pre-existing profile file. Administrator privileges required to replace it" 
+        check_sudo
+        rm /etc/profile
+    fi
+    ln profile /etc/profile
     ln bashrc_docker ~/.bashrc_docker
 fi
 
@@ -21,7 +36,7 @@ source ~/.bashrc
 
 # SSH
 if [ -d "$HOME/.ssh" ]; then
-    echo "Found pre-existing .ssh/config. Removing it"
+    echo "Found pre-existing .ssh/config. Replacing it"
     rm ~/.ssh/config
     ln ssh_config ~/.ssh/config
 fi
