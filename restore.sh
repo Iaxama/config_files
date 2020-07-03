f7e0e176eb54275626ef8f9677bbf789e3643b4b
Massimiliano Iacono <massimiliano.iacono@iit.it>
2020-07-03 10:54:47 +0200

    Added message found docker

--- restore.sh
+++ restore.sh
@@ -18,6 +18,7 @@ fi
 ln bashrc ~/.bashrc
 
 if [ `is_installed docker-ce-cli` -eq "1" ]; then
+    echo "Found Docker installation. Adding command line helpers"
     if [ -f "$HOME/.bashrc_docker" ]; then
         echo "Found pre-existing bashrc_docker. Replacing it" 
         rm ~/.bashrc_docker
