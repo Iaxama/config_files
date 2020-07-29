522ba5affadd0e40927056c1a8cb99670dc03541
Massimiliano Iacono <massimiliano.iacono@iit.it>
2020-07-29 10:11:51 +0200

    Fixed bug when using restore with sudo

--- restore.sh
+++ restore.sh
@@ -1,43 +1,45 @@
 #!/bin/bash
 
-check_sudo(){
-    if [[ $UID != 0 ]]; then
-        echo "Please run this script with sudo:"
-        echo "sudo $0 $*"
-        exit 1
-    fi
-}
+if [[ $UID == 0 ]]; then
+    HOME=$(getent passwd $SUDO_USER | cut -d: -f6)
+fi
 
 . bashrc
 
 # BASHRC
 if [ -f "$HOME/.bashrc" ]; then
     echo "Found pre-existing bashrc. Replacing it" 
-    rm ~/.bashrc
+    rm $HOME/.bashrc
 fi
-ln bashrc ~/.bashrc
+ln bashrc $HOME/.bashrc
 
 if [ `is_installed docker-ce-cli` -eq "1" ]; then
     echo "Found Docker installation. Adding command line helpers"
     if [ -f "$HOME/.bashrc_docker" ]; then
         echo "Found pre-existing bashrc_docker. Replacing it" 
-        rm ~/.bashrc_docker
+        rm $HOME/.bashrc_docker
     fi
+        ln bashrc_docker $HOME/.bashrc_docker
 
-    if [ -f "/etc/profile" ]; then
-        echo "Found pre-existing profile file. Administrator privileges required to replace it" 
-        check_sudo
-        rm /etc/profile
+    if [[ $UID == 0 ]]; then
+        
+        if [ -f "/etc/profile" ]; then
+            echo "Found pre-existing profile file"
+            rm /etc/profile
+        fi
+            ln profile /etc/profile
+
+    else
+        echo "WARNING: Administrator privileges required to replace /etc/profile. Ignoring it."
+
     fi
-    ln profile /etc/profile
-    ln bashrc_docker ~/.bashrc_docker
 fi
 
-source ~/.bashrc
+source $HOME/.bashrc
 
 # SSH
 if [ -d "$HOME/.ssh" ]; then
     echo "Found pre-existing .ssh/config. Replacing it"
-    rm ~/.ssh/config
-    ln ssh_config ~/.ssh/config
+    rm $HOME/.ssh/config
+    ln ssh_config $HOME/.ssh/config
 fi
